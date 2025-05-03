5b15d9892e4be5041c4a6de63893c81aa29b6c96
darkitote <darckgamer_2002@hotmail.com>
2025-05-03 04:06:12 -0600

    f

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  base: "/PokeCare-main/",
   server: {
-    port: 3000, // Asegura que coincida con el puerto que exponemos en Docker
-    host: "0.0.0.0", // Permite conexiones desde fuera del contenedor
-  }
+    port: 3000,
+    host: "0.0.0.0", // Permite conexiones externas
+    hmr: {
+      clientPort: 443, // Si usas HTTPS en Minikube, ajusta el puerto
+    },
+  },
+  base: "/",
 });
